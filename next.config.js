970b24f501feb4c9181232108c9947e2b7ad637b
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2023-01-22 13:12:03 +0700

    feat: add storage domain to next image

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost", "127.0.0.1"],
+    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost", "127.0.0.1", "storage.localvocal.id"],
   },
 })
 
